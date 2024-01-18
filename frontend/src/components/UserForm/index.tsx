2fab0a7519ed4d7c0a577118c4629aeb41e081fb
Darlan0307 <darlanchagas2020@gmail.com>
2024-01-18 10:41:10 -0300

    :lipstick: formulario criado

--- frontend/src/components/UserForm/index.tsx
+++ frontend/src/components/UserForm/index.tsx
@@ -2,7 +2,70 @@ import './styles.scss'
 
 const UserForm = () => {
   return (
-    <div>UserForm</div>
+    <div className='container-userform'>
+      <div className='content-header'>
+        <h2>Pesquisa Rápida!</h2>
+        <p>Queremos saber sua relação com a tecnologia</p>
+      </div>
+      <form className='form'>
+        <div className='wrap-input'>
+          <input type="text" placeholder='Nome' required/>
+        </div>
+        <div className='wrap-input'>
+          <input type="email" placeholder='Email' required/>
+        </div>
+        <div className='wrap-input-radio'>
+          <span className='question'>Você usa tecnologia no dia a dia?</span>
+          <div className='alternative'>
+            <div>
+              <input type="radio" id='sim' name='radio1' required/>
+              <label htmlFor="sim">Sim</label>
+            </div>
+            <div>
+              <input type="radio" id='nao' name='radio1'  required/>
+              <label htmlFor="nao">Não</label>
+            </div>
+          </div>
+        </div>
+
+        <div className='wrap-input-radio'>
+          <span className='question'>Você trabalharia com tecnologia?</span>
+          <div className='alternative'>
+            <div>
+              <input type="radio" id='sim-2' name='radio2' required/>
+              <label htmlFor="sim-2">Sim</label>
+            </div>
+            <div>
+              <input type="radio" id='nao-2' name='radio2' required/>
+              <label htmlFor="nao-2">Não</label>
+            </div>
+          </div>
+        </div>
+
+        <div className='wrap-input-radio'>
+          <span className='question'>Você concorda que a tecnologia é importante?</span>
+          <div className='alternative'>
+            <div>
+              <input type="radio" id='sim-3' name='radio3' required/>
+              <label htmlFor="sim-3">Sim</label>
+            </div>
+            <div>
+              <input type="radio" id='nao-3' name='radio3' required/>
+              <label htmlFor="nao-2">Não</label>
+            </div>
+          </div>
+        </div>
+
+        <div className='wrap-input-text'>
+          <span className='question'>Fale oque você mais gosta em relação a tecnologia</span>
+          
+            <textarea name="comment" id="comment" placeholder='sua resposta' required></textarea>
+          
+        </div>
+        
+        <button className='btn' type='submit'> Enviar </button>
+      </form>
+    </div>
   )
 }
 
