85ba42c4ab576a4d3f31bc95116bf1999aab2307
Darlan0307 <darlanchagas2020@gmail.com>
2024-01-18 11:16:27 -0300

    :lipstick: animação adicionada

---
+++ frontend/src/components/Lottie/LottieAnimation/index.tsx
@@ -0,0 +1,23 @@
+import Lottie from 'lottie-web';
+import { useEffect, useRef } from 'react';
+import animationData from '../../../Animations/AnimateRobo.json'; // Carregue o arquivo JSON da animação aqui
+
+const LottieAnimation = () => {
+  const lottieContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const instance = Lottie.loadAnimation({
+      container: lottieContainer.current as HTMLDivElement,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData,
+    });
+
+    return () => instance.destroy();
+  }, []);
+
+  return <div ref={lottieContainer} style={{ width: '100%', height: '100%' }} />;
+};
+
+export default LottieAnimation;
